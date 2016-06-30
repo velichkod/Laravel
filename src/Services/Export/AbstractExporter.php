9d1678da3984646dffe6df4c6981b986a5a598fd
themightysapien <themightysapien@gmail.com>
2016-06-30 11:45:44 +0545

    new changes

---
+++ src/Services/Export/AbstractExporter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Optimait\Laravel\Services;
+
+
+abstract class AbstractExporter {
+
+    protected $heading;
+    protected $filename;
+
+    public function setHeadings($array){
+        $this->heading = $array;
+        return $this;
+    }
+
+    public function setName($name){
+        $this->filename = $name;
+        return $this;
+    }
+    public abstract function export($data);
+} 
---
+++ src/Services/Export/SampleExporter.php
@@ -0,0 +1,50 @@
+<?php
+namespace Optimait\Laravel\Services;
+
+use Response;
+
+class SampleExporter extends AbstractExporter implements ExportInterface
+{
+
+    public function export($data)
+    {
+        $output = '';
+        //First lets get the headings
+        $output .= implode(',', $this->heading) . "\n";
+
+        foreach ($data['order_id'] as $k => $order) {
+            $output .= $this->escapeAndReturn(array(
+                $order,
+                $data['customer'][$k],
+                $data['campaign'][$k],
+                $data['gateway'][$k],
+                $data['product'][$k],
+                $data['price'][$k],
+                $data['tracking'][$k],
+
+            ));
+
+
+            $output .= "\n";
+        }
+        $filename = $this->filename != '' ? $this->filename : 'Exports-' . @date('Y-m-d-H-i-s');
+        $headers = array(
+            'Content-Type' => 'text/csv',
+            'Content-Disposition' => 'attachment; filename="' . $filename . '.csv"',
+        );
+
+        return Response::make($output, 200, $headers);
+    }
+
+    public function escapeAndReturn($array)
+    {
+        $str = '';
+        foreach ($array as $val) {
+            $str .= str_replace(array("\r\n", "\n", "\r", ","), array(" ", " ", " ", " "), $val) . ",";
+
+        }
+        return $str;
+    }
+
+
+}
