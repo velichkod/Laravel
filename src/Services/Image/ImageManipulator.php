a3a242cd40bd50764acecd16e566be7d273369f7
themightysapien <themightysapien@gmail.com>
2017-01-29 19:26:22 +0545

    image manipulator changes

--- src/Services/Image/ImageManipulator.php
+++ src/Services/Image/ImageManipulator.php
@@ -4,34 +4,43 @@ namespace Optimait\Laravel\Services\Image;
 use \Image;
 
 
-class ImageManipulator {
+class ImageManipulator
+{
 
 
-    public function resize($filename, $uploadFolder){
+    public function resize($filename, $uploadFolder)
+    {
 
         $sizes = array_values(config('resize.sizes'));
 
-        foreach($sizes as $size){
-            $img = Image::make($uploadFolder.$filename);
-            if(isset($size[2])){
-                if($size[2] == 'width'){
-                    $img->resize(null, $size[1], function ($constraint) {
-                        $constraint->aspectRatio();
-                        //$constraint->upsize();
-                    });
+        foreach ($sizes as $size) {
+            $img = Image::make($uploadFolder . $filename);
+            if (isset($size[2])) {
+                switch ($size[2]) {
+                    case 'width':
+                        $img->resize(null, $size[1], function ($constraint) {
+                            $constraint->aspectRatio();
+                            //$constraint->upsize();
+                        });
+                        break;
+
+                    case 'height':
+                        $img->resize($size[0], null, function ($constraint) {
+                            $constraint->aspectRatio();
+                            //$constraint->upsize();
+                        });
+                        break;
+
+                    case 'fit':
+                        $img->fit($size[0], $size[1]);
+                        break;
                 }
-                else{
-                    $img->resize($size[0], null, function ($constraint) {
-                        $constraint->aspectRatio();
-                        //$constraint->upsize();
-                    });
-                }
-            }
-            else{
+
+            } else {
                 $img->resize($size[0], $size[1]);
             }
 
-            $img->save($uploadFolder.$size[0].'X'.$size[1].$filename);
+            $img->save($uploadFolder . $size[0] . 'X' . $size[1] . $filename);
         }
 
     }
