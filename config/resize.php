08fba3e1a856af3bc2e84c672279c12fd1cee9b5
themightysapien <themightysapien@gmail.com>
2017-01-29 19:32:44 +0545

    resize config minor fix

--- config/resize.php
+++ config/resize.php
@@ -2,6 +2,6 @@
 return [
     'sizes' => [
         'thumb' => array(100,100, 'height'),
-        'medium' => array(250,150, 'height')/*, array(270, 150), array(600,350,'height')*/
+        'medium' => array(250,250, 'fit')/*, array(270, 150), array(600,350,'height')*/
     ]
 ];
