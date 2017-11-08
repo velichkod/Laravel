af0b3d7c2883a8943f80b6a3abca2943ba0dfa5d
themightysapien <themightysapien@gmail.com>
2017-11-08 14:39:49 +0545

    Update helpers.php
    updating function declarations

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -3,7 +3,8 @@
  * @param $string
  * @return string
  */
-function encryptIt($string){
+function encryptIt($string)
+{
     return urlencode(Crypt::encrypt($string));
 }
 
@@ -11,7 +12,8 @@ function encryptIt($string){
  * @param $encrypted
  * @return string
  */
-function decryptIt($encrypted){
+function decryptIt($encrypted)
+{
     return Crypt::decrypt(urldecode($encrypted));
 }
 
@@ -20,28 +22,63 @@ function decryptIt($encrypted){
  * @param array $params
  * @return string
  */
-function sysRoute($route, $params=array()){
-    return route('webpanel.'.$route, $params);
-}
+if (!function_exists('sysRoute')):
+    function sysRoute($route, $params = array())
+    {
+        return route('webpanel.' . $route, $params);
+    }
+endif;
 
 /**
  * @param $url
  * @return string
  */
-function sysUrl($url, $params = array()){
-    return url('webpanel/'.$url, $params);
-}
+if (!function_exists('sysUrl')):
+    function sysUrl($url, $params = array())
+    {
+        return url('webpanel/' . $url, $params);
+    }
+endif;
 
 /**
  * @param $view
  * @param array $data
  * @return \Illuminate\View\View
  */
-function sysView($view, $data = array()){
-    return view('webpanel.'.$view, $data);
-}
+if (!function_exists('sysView')):
+    function sysView($view, $data = array())
+    {
+        return view('webpanel.' . $view, $data);
+    }
+endif;
 
 
-function frontView($view, $data = array()){
-    return view('frontend.'.$view, $data);
-}
+if (!function_exists('frontView')):
+    function frontView($view, $data = array())
+    {
+        return view('frontend.' . $view, $data);
+    }
+endif;
+
+
+if (!function_exists('frontUrl')):
+    function frontUrl($urL)
+    {
+        return url('frontpanel/' . $urL);
+    }
+endif;
+
+if (!function_exists('frontRoute')):
+    function frontRoute($route, $data = [])
+    {
+        return route('frontpanel.' . $route, $data);
+    }
+endif;
+
+
+if (!function_exists('isAction')):
+    function isAction($action)
+    {
+        return \Route::getCurrentRoute()->getActionName() == 'App\Http\Controllers\\' . $action;
+    }
+endif;
